a41e614c94dfa2f678ff5e7a552bff131aaa6d9d
Byrne Yan <byrne.yan@yahoo.com>
2020-01-05 22:25:48 +0800

    draft

---
+++ src/main/java/com/byrne/cfts/CftsApplication.java
@@ -0,0 +1,13 @@
+package com.byrne.cfts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CftsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CftsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/byrne/cfts/CftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byrne.cfts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CftsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
